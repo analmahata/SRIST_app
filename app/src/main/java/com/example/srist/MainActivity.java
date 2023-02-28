48dbd1c94435567224b0f1d5d8019688bce7a22d
Soumya Baidya <developer.baidya@gmail.com>
2023-03-01 01:26:50 +0530

    toolbar, main page menu

--- app/src/main/java/com/example/srist/MainActivity.java
+++ app/src/main/java/com/example/srist/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.srist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+
+        findViewById(R.id.imageProfilelogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.END);
+
+            }
+        });
     }
 }
